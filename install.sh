548b7f8828a54ac8e8ca498e98808281cd884daa
[ hacker-unknon ] <40244622+th3unkn0n@users.noreply.github.com>
2019-08-24 19:38:48 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [[ command -v tor > /dev/null 2>&1;
  	command -v torsocks > /dev/null 2>&1 ]]; then
  		clear && echo "[+] All Done !!\n[+] You Can Run ./facebash.sh"
  	else pkg upgrade -y && pkg update -y
- 		clear && echo "[+] Installing Requierd Files..."
- 		pkg install -y tor curl torsocks >> /dev/null
+ 		clear && echo "[+] Installing Requierd Files..." && sleep 5
+ 		pkg install -y tor curl torsocks
  		clear && echo "[+] All Done !!\n[+] You Can Run ./facebash.sh"
 fi
